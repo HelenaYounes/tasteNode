f4ec88c8c145afe08db875639a774e4fd357ad94
Héléna Younes <younesq.helena@gmail.com>
2018-04-12 15:28:01 -0700

    add asynch request

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+const fetch = require('node-fetch');
 const express = require('express');
-
 const app = express();
-app.get('/',(request, response) => {
-  response.send('hello woooorld');
+
+app.get('/', (request, response) => {
+  const url = 'https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction&k=305528-helenayo-DC3O5ZEW';
+  fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      // console.log(json));
+      response.send(json)
+    });
 });
 
 app.listen(3000);
