122d4b3374d37abcede3dcc79b3ba60755c56209
Héléna Younes <younesq.helena@gmail.com>
2018-04-14 16:02:39 -0700

    get suggestion string from url

--- server/app.js
+++ server/app.js
@@ -1,34 +1,33 @@
 const fetch = require('node-fetch');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
+const url = "https://tastedive.com/api/similar?k=305528-helenayo-DC3O5ZEW&q="
 
 app.use(express.static('dist'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/suggestionsData', (request, response) => {
-  const url = 'https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction&k=305528-helenayo-DC3O5ZEW';
-  fetch(url)
-    .then(res => res.json())
-    .then(json => response.render('templates/suggestionsList', {results: json.Similar.Results}));
-});
+app.use(cookieParser());
 
-app.get('/', function (req, res) {
-  res.render('index', { title: 'TasteDiveAPI'})
-});
-
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.post('/suggestions', (req, res) => {
-  console.log(req.body);
-  res.render('templates/suggestionsList');
+app.post('/suggestion', (req, response) => {
+  const id = req.body.suggestion
+  response.redirect(`suggestion/${id}`)
+});
+
+app.get('/suggestion/:id', (req, res) => {
+  const id = req.params.id;
+  const query = url + id;
+  fetch(query)
+    .then(response => response.json())
+    .then(json => res.render('suggestionsList', {results: json.Similar.Results}));
 });
 
 app.set('view engine', 'pug');
-
-
 
 app.listen(5000);
